c8ee64b7c1764a32cc3d97bd38e77ab1d9bfa5a6
juanituza <juanituza85@gmail.com>
2024-04-29 09:31:11 -0300

    modificacion aside admin panel

--- src/public/js/nuevoArchivo.js
+++ src/public/js/nuevoArchivo.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const menuToggleBtn = document.querySelector(".layout-menu .menu-toggle-btn");
   const layoutMenu = document.querySelector(".layout-menu");
   const layoutPage = document.querySelector(".layout-page");
-  const page = document.querySelector("#page");
+  const table = document.querySelector("#miTabla");
 
-  if (menuToggleBtn && layoutMenu && layoutPage && page) {
+  if (menuToggleBtn && layoutMenu && layoutPage && table) {
     menuToggleBtn.addEventListener("click", function (event) {
       event.stopPropagation();
 
@@ -17,18 +17,18 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 
-    // Ajustar el margen izquierdo inicial del contenido
-    layoutPage.style.marginLeft = "200px"; // Ajusta el margen para tener en cuenta el aside cuando se carga la página
-
-    document.addEventListener("click", function (event) {
-      // Verificar si el clic proviene del área del aside
-      if (!layoutMenu.contains(event.target)) {
-        // Si no, contraer el aside
-        layoutMenu.classList.add("menu-collapsed");
-        layoutPage.style.marginLeft = "50px"; // Mantener el margen izquierdo fijo
-      }
+    // Detener la propagación del clic en la tabla para evitar que llegue al aside
+    table.addEventListener("click", function (event) {
+      event.stopPropagation();
     });
   } else {
     console.error("One or more required elements not found.");
   }
+
+  document.addEventListener("click", function () {
+    if (layoutMenu.classList.contains("menu-collapsed")) {
+      layoutMenu.classList.remove("menu-collapsed");
+      layoutPage.style.marginLeft = "200px";
+    }
+  });
 });
